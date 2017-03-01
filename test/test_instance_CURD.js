548542c8eb02dd3f4b8695981e5bdce6d66804d1
PandaFeeder <pandafeeder@gmail.com>
2017-03-02 03:01:23 +0800

    update test

--- test/test_instance_CURD.js
+++ test/test_instance_CURD.js
@@ -76,7 +76,7 @@ describe('test for instance\' CURD opeartion', function() {
         done()
       })
     })
-    it('new instance with arguments, this should successfully saved', function(done) {
+    it('new instance with arguments, this should successfully saved, another save should call update', function(done) {
       let book = new Books1({
         title: '时间的朋友',
         author: '李笑来',
@@ -84,6 +84,37 @@ describe('test for instance\' CURD opeartion', function() {
         price: 10.5,
         keywords: ['time', 'life']
       })
+      book.save().then(r => {
+        assert.ok(r.insertedCount === 1)
+        book.price = 11.5
+        book.save().then(r => {
+          assert.ok(r.modifiedCount === 1)
+          done()
+        })
+      })
+    })
+    it('call update before save should throw error', function(){
+      let book = new Books1({
+        title: '时间的朋友',
+        author: '李笑来',
+        publish: new Date(),
+        price: 10.5,
+        keywords: ['time', 'life']
+      })
+      assert.throws(() => {
+        book.update()
+      }, Error)
+    })
+    it('new instance without arguments, add data using addData function, then save it', function(done) {
+      let book = new Books1()
+      book.addData({
+        title: 'Conversation in the Cathedral',
+        author: 'Mario Vargas Llosa',
+        publish: new Date(1969),
+        price: 30.5,
+        keywords: ['novel']
+      })
+      assert.ok(book.title === 'Conversation in the Cathedral')
       book.save().then(r => {
         assert.ok(r.insertedCount === 1)
         done()
@@ -96,4 +127,45 @@ describe('test for instance\' CURD opeartion', function() {
   //  it('', function() {
   //  })
   //})
+  describe('static class methods for CURD', function() {
+    it('findOneAndNew should return a newly created instance, then update it', function(done) {
+      Books1.findOneAndNew({title: 'Conversation in the Cathedral'}).then(obj => {
+        assert.ok(obj.__saved === true)
+        obj.price = 40.5
+        obj.save().then(r => {
+          assert.ok(r.modifiedCount === 1)
+          done()
+        })
+      })
+    })
+    it('it should throw error when calling insert which is deprecated', function() {
+      assert.throws(() => {
+        Books1.insert()
+      },Error)
+    })
+    it('insertOne should just call native insertOne to do the CREATE opeartion, should pay attension that when insert this way, constrains on schema is not checked', function(done) {
+      Books1.insertOne({
+        title: '7 databases in 7 weeks',
+        author: 'Eric Redmond and Jim R. Wilson',
+        publish: new Date(2012,4,11),
+        price: 35,
+      }).then(r => {
+        assert.ok(r.insertedCount === 1)
+        done()
+      })
+    })
+    it('test class\'s insertMany', function(done) {
+      let data = [{title: 't1', publish: 'p1'},{title: 't2', publish: 't2'},{title: 't3', publish: 't3'}]
+      Books1.insertMany(data)
+        .then(r => {
+          assert.ok(r.insertedCount === data.length)
+          done()
+        })
+    })
+    it('this should throw error when using class\'s static findAndModify which is depreacted by native driver', function() {
+      assert.throws(() => {
+        Books1.findAndModify()
+      }, Error)
+    })
+  })
 })
