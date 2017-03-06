66bd9012b4c4ec580d4a2729fe704f882ef4c694
PandaFeeder <pandafeeder@gmail.com>
2017-03-07 02:44:37 +0800

    add for testing embedded doc

---
+++ test/books3.js
@@ -0,0 +1,25 @@
+'ues strict'
+const DOC = require('../src').DOC
+
+class Address extends DOC {
+  constructor(data) {
+    super(data)
+    this.setSchema({
+      country: String,
+      city: String,
+      zip: types.Int
+    })
+  }
+}
+
+
+class Person extends DOC {
+  constructor(data) {
+    super(data)
+    this.setSchema({
+      name: String,
+      address: 'Address',
+      extra: Object
+    })
+  }
+}
