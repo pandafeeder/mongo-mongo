a381ce39e9cc3d4f4e605aea45733a406462cf26
PandaFeeder <pandafeeder@gmail.com>
2017-03-08 01:03:21 +0800

    main entry

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const DOC = require('./src/doc')
+const DB = require('./src/db')
+const types = require('./src/types')
+
+module.exports = exports = {
+  DOC: DOC,
+  DB: DB,
+  types: types
+}
