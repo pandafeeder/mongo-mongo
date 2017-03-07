f96cf93dee5dc570751cd14c18c2adaabcd3c1c9
PandaFeeder <pandafeeder@gmail.com>
2017-03-08 01:04:17 +0800

    make Int number pass Float type-constrain

--- src/types.js
+++ src/types.js
@@ -7,7 +7,7 @@ function Int (dataField) {
 }
 
 function Float(dataField) {
-  if (dataField % 1 === 0) {
+  if ((dataField % 1 === 0)&&(Math.floor(dataField) !== dataField)) {
     throw new Error(`${dataField} is not Float`)
   }
 }
